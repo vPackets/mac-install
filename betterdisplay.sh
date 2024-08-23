9dec8978d1ed9bb8faba7de999a4a896778a9079
Nicolas Michel <Nicolas@vpackets.net>
2024-08-23 17:05:37 -0500

    added betterdisplay cli

---
+++ betterdisplay.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+betterdisplaycli set --name="LG HDR 4K" --displayModeNumber=63
+sleep 1
+betterdisplaycli set --name="CS-DESKPRO-2" --displayModeNumber=33
+sleep 1
+betterdisplaycli set --name="CS-DESKPRO-2" --displayModeNumber=59
+
+
